bce7f0969bc2c2a0de2761aca39f7c2f578819b7
Afzal Ahmad <afzalhmd14@gmail.com>
2025-01-06 17:53:22 +0100

    udpate: switched to ros2 humble

--- mount/99-env.sh
+++ mount/99-env.sh
@@ -3,9 +3,8 @@
 CUSTOM_LOCATION=/opt/env
 
 # link bash and zsh rc files
-[ ! -e ~/.bashrc ] &&  ln -s $CUSTOM_LOCATION/host/apptainer_bashrc.sh ~/.bashrc
-# [ ! -e ~/.zshrc ] && ln -s $CUSTOM_LOCATION/host/apptainer_zshrc.sh ~/.zshrc
-[ ! -e ~/.profile ] && ln -s $CUSTOM_LOCATION/host/apptainer_profile.sh ~/.profile
+[ ! -e ~/.zshrc ] && ln -s $CUSTOM_LOCATION/host/apptainer_zshrc.sh ~/.zshrc
+# [ ! -e ~/.profile ] && ln -s $CUSTOM_LOCATION/host/apptainer_profile.sh ~/.profile
 
 # touch ~/.sudo_as_admin_successful
 
