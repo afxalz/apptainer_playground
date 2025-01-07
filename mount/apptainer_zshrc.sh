8fa249f0657efabf34a2660bd1dfbd3c8d0d6615
Afzal Ahmad <afzalhmd14@gmail.com>
2025-01-07 18:11:45 +0100

    update: working version with tmux and zsh

--- mount/apptainer_zshrc.sh
+++ mount/apptainer_zshrc.sh
@@ -1,9 +1,9 @@
 export SHELL=$( which zsh )
 
-PROMPT='[Apptainer]%1~ %# '
+
 
 # ROS2 env-varibles
-source /opt/ros/humble/setup.bash
+source /opt/ros/humble/setup.zsh
 export ROS_LOCALHOST_ONLY=1
 export RMW_IMPLEMENTATION=rmw_fastrtps_cpp
 export FASTDDS_DEFAULT_PROFILES_FILE=/opt/env/host/ros2/DEFAULT_FASTRTPS_PROFILES_FW.xml
@@ -14,9 +14,19 @@ alias clb='colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=RelWit
 # source the user_workspace, if it exists
 [ -e ~/user_ros_workspace/install/setup.zsh ] && source ~/user_ros_workspace/install/setup.zsh
 
-# source the linux setup from within
-if [ -e /opt/env/host/custom_configs/zsh/dotzshrc ]; then
+if [ -e /opt/env/host/config/dotzshrc ]; then
 
-  source /opt/env/host/custom_configs/zsh/dotzshrc
+  source /opt/env/host/config/dotzshrc
 
 fi
+
+# set variable identifying the chroot you work in (used in the prompt below)
+if [ -z "${debian_chroot:-}" ] && [ -r /etc/debian_chroot ]; then
+    debian_chroot=$(cat /etc/debian_chroot)
+fi
+
+# some more ls aliases
+alias ll='ls -alF'
+alias la='ls -A'
+alias l='ls -CF'
+PROMPT='[Apptainer]'$PROMPT
