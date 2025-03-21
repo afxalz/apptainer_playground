14e49f47afd15b7497c319505705f9e7ffecf4b5
Afzal Ahmad <afzalhmd@gmail.com>
2025-03-21 23:21:46 +0100

    fixed: zshrc was using bad names

--- mount/apptainer_zshrc.sh
+++ mount/apptainer_zshrc.sh
@@ -6,15 +6,13 @@ alias la='ls -A'
 alias l='ls -CF'
 
 if [ -e /opt/env/host/config/dotzshrc ]; then
-
   source /opt/env/host/config/dotzshrc
-
 fi
 
 # prefix the shell prompt with Apptainer
 PROMPT='[Apptainer]'$PROMPT
 
-ros2() {
+ros2_jazzy_env() {
   # ROS2 env-varibles
   source /opt/ros/${ROS_DISTRO}/setup.zsh
   export ROS_AUTOMATIC_DISCOVERY_RANGE=LOCALHOST
@@ -25,27 +23,31 @@ ros2() {
   alias clbt='colcon build --packages-up-to $(basename `pwd`)'
   alias clb='colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=RelWithDebInfo'
 
+  echo -e "Sourced ROS2-$ROS_DISTRO env"
+
   # source the user_workspace, if it exists
-  [ -e ~/user_ros_workspace/install/setup.zsh ] && source ~/user_ros_workspace/install/setup.zsh
-  [ -e ~/user_ros_workspace/src/mrs_ros2_ws/install/setup.zsh ] && source ~/user_ros_workspace/src/mrs_ros2_ws/install/setup.zsh
+  # [ -e ~/user_ros_workspace/install/setup.zsh ] && source ~/user_ros_workspace/install/setup.zsh
+  # [ -e ~/user_ros_workspace/src/mrs_ros2_ws/install/setup.zsh ] && source ~/user_ros_workspace/src/mrs_ros2_ws/install/setup.zsh
 }
 
-ros1() {
+ros1_noetic_env() {
   # ROS2 env-varibles
   source /opt/ros/${ROS_DISTRO}/setup.zsh
 
   alias clbt='catkin bt'
   alias clb='catkin build'
 
+  echo -e "Sourced ROS2-$ROS_DISTRO env"
+
   # source the user_workspace, if it exists
-  [ -e ~/user_ros_workspace/devel/setup.zsh ] && source ~/user_ros_workspace/devel/setup.zsh
+  # [ -e ~/user_ros_workspace/devel/setup.zsh ] && source ~/user_ros_workspace/devel/setup.zsh
 }
 
 if [[ "$ROS_DISTRO" =~ "noetic" ]]; then
-  ros1
+  ros1_noetic_env
 fi
 if [[ "$ROS_DISTRO" =~ "jazzy" ]]; then
-  ros2
+  ros2_jazzy_env
 fi
 
 
