b349aef507ca8941e9591b24dbedd4cdb8e944ec
Afzal Ahmad <afzalhmd@gmail.com>
2025-03-21 20:42:53 +0100

    updated: mounted files and ros1-noetic recipe

--- mount/apptainer_zshrc.sh
+++ mount/apptainer_zshrc.sh
@@ -14,16 +14,38 @@ fi
 # prefix the shell prompt with Apptainer
 PROMPT='[Apptainer]'$PROMPT
 
-# ROS2 env-varibles
-source /opt/ros/jazzy/setup.zsh
-export ROS_AUTOMATIC_DISCOVERY_RANGE=LOCALHOST
-export ROS_STATIC_PEERS=''
-export RMW_IMPLEMENTATION=rmw_fastrtps_cpp
-export FASTDDS_DEFAULT_PROFILES_FILE=/opt/env/host/ros2/DEFAULT_FASTRTPS_PROFILES_FW.xml
+ros2() {
+  # ROS2 env-varibles
+  source /opt/ros/${ROS_DISTRO}/setup.zsh
+  export ROS_AUTOMATIC_DISCOVERY_RANGE=LOCALHOST
+  export ROS_STATIC_PEERS=''
+  export RMW_IMPLEMENTATION=rmw_fastrtps_cpp
+  export FASTDDS_DEFAULT_PROFILES_FILE=/opt/env/host/ros2/DEFAULT_FASTRTPS_PROFILES_FW.xml
 
-alias clbt='colcon build --packages-up-to $(basename `pwd`)'
-alias clb='colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=RelWithDebInfo'
+  alias clbt='colcon build --packages-up-to $(basename `pwd`)'
+  alias clb='colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=RelWithDebInfo'
 
-# source the user_workspace, if it exists
-[ -e ~/user_ros_workspace/install/setup.zsh ] && source ~/user_ros_workspace/install/setup.zsh
-[ -e ~/user_ros_workspace/src/mrs_ros2_ws/install/setup.zsh ] && source ~/user_ros_workspace/src/mrs_ros2_ws/install/setup.zsh
+  # source the user_workspace, if it exists
+  [ -e ~/user_ros_workspace/install/setup.zsh ] && source ~/user_ros_workspace/install/setup.zsh
+  [ -e ~/user_ros_workspace/src/mrs_ros2_ws/install/setup.zsh ] && source ~/user_ros_workspace/src/mrs_ros2_ws/install/setup.zsh
+}
+
+ros1() {
+  # ROS2 env-varibles
+  source /opt/ros/${ROS_DISTRO}/setup.zsh
+
+  alias clbt='catkin bt'
+  alias clb='catkin build'
+
+  # source the user_workspace, if it exists
+  [ -e ~/user_ros_workspace/devel/setup.zsh ] && source ~/user_ros_workspace/devel/setup.zsh
+}
+
+if [[ "$ROS_DISTRO" =~ "noetic" ]]; then
+  ros1
+fi
+if [[ "$ROS_DISTRO" =~ "jazzy" ]]; then
+  ros2
+fi
+
+
