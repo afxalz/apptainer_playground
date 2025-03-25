b2bb71080c531dd36ca70ec341a1d2516692560f
Afzal Ahmad <afzalhmd14@gmail.com>
2025-03-25 19:06:55 +0100

    wip: adding starship and fixing zsh

--- mount/99-env.sh
+++ mount/99-env.sh
@@ -4,14 +4,15 @@ CUSTOM_LOCATION=/opt/env
 
 # link bash and zsh rc files
 [ ! -e ~/.zshrc ] && ln -s $CUSTOM_LOCATION/host/apptainer_zshrc.sh ~/.zshrc
+[ ! -e ~/.config/starship.toml ] && ln -s $CUSTOM_LOCATION/host/config/starship.toml ~/.config/starship.toml
 
 # [ ! -e ~/.vim ] && ln -s $CUSTOM_LOCATION/host/configs/dotvim ~/.vim
 
-[ ! -e ~/.vimrc ] && ln -s $CUSTOM_LOCATION/host/config/dotvimrc ~/.vimrc
+# [ ! -e ~/.vimrc ] && ln -s $CUSTOM_LOCATION/host/config/dotvimrc ~/.vimrc
 
 # link tmux conf
-[ ! -e ~/.tmux.conf ] && ln -s $CUSTOM_LOCATION/host/config/dottmux.conf ~/.tmux.conf
+# [ ! -e ~/.tmux.conf ] && ln -s $CUSTOM_LOCATION/host/config/dottmux.conf ~/.tmux.conf
 [ ! -d ~/.oh-my-zsh ] && ln -s /opt/oh-my-zsh ~/.oh-my-zsh
 
 # touch ~/.sudo_as_admin_successful
-export PS1="[Apptainer] ${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ "
+# export PS1="${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\][Apptainer]\$ "
--- mount/apptainer_zshrc.sh
+++ mount/apptainer_zshrc.sh
@@ -10,21 +10,19 @@ if [ -e /opt/env/host/config/dotzshrc ]; then
 fi
 
 # prefix the shell prompt with Apptainer
-PROMPT='[Apptainer]'$PROMPT
+PROMPT=$PROMPT'[Apptainer]'
+
+source /opt/ros/${ROS_DISTRO}/setup.zsh
 
 ros2_jazzy_env() {
   # ROS2 env-varibles
-  source /opt/ros/${ROS_DISTRO}/setup.zsh
   export ROS_AUTOMATIC_DISCOVERY_RANGE=LOCALHOST
   export ROS_STATIC_PEERS=''
   export RMW_IMPLEMENTATION=rmw_fastrtps_cpp
   export FASTDDS_DEFAULT_PROFILES_FILE=/opt/env/host/ros2/DEFAULT_FASTRTPS_PROFILES_FW.xml
 
   alias clbt='colcon build --packages-up-to $(basename `pwd`)'
-  alias clb='colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=RelWithDebInfo'
-
-  echo -e "Sourced ROS2-$ROS_DISTRO env"
-
+  alias clb='colcon build'
   # source the user_workspace, if it exists
   # [ -e ~/user_ros_workspace/install/setup.zsh ] && source ~/user_ros_workspace/install/setup.zsh
   # [ -e ~/user_ros_workspace/src/mrs_ros2_ws/install/setup.zsh ] && source ~/user_ros_workspace/src/mrs_ros2_ws/install/setup.zsh
@@ -37,10 +35,9 @@ ros1_noetic_env() {
   alias clbt='catkin bt'
   alias clb='catkin build'
 
-  echo -e "Sourced ROS2-$ROS_DISTRO env"
-
   # source the user_workspace, if it exists
-  # [ -e ~/user_ros_workspace/devel/setup.zsh ] && source ~/user_ros_workspace/devel/setup.zsh
+  [ -e ~/workspaces/indair_ws/devel/setup.zsh ] && source ~/workspaces/indair_ws/devel/setup.zsh
+  # [ -e ~/workspaces/indair/devel/setup.zsh ] && source ~/workspaces/indair/devel/setup.zsh
 }
 
 if [[ "$ROS_DISTRO" =~ "noetic" ]]; then
@@ -49,5 +46,4 @@ fi
 if [[ "$ROS_DISTRO" =~ "jazzy" ]]; then
   ros2_jazzy_env
 fi
-
-
+echo -e "Sourced ROS-$ROS_DISTRO env"
