97c7f381368cdd615b9013edff6ce095a4488c42
Afzal Ahmad <afzalhmd14@gmail.com>
2025-03-27 11:15:45 +0100

    added: new tmux conf and theme pack with gitman

--- mount/99-env.sh
+++ mount/99-env.sh
@@ -4,13 +4,14 @@ CUSTOM_LOCATION=/opt/env
 
 # link bash and zsh rc files
 [ ! -e ~/.zshrc ] && ln -s $CUSTOM_LOCATION/host/apptainer_zshrc.sh ~/.zshrc
+[ ! -d ~/.oh-my-zsh ] && ln -s /opt/oh-my-zsh ~/.oh-my-zsh
 
 # [ ! -e ~/.vim ] && ln -s $CUSTOM_LOCATION/host/configs/dotvim ~/.vim
 
 # [ ! -e ~/.vimrc ] && ln -s $CUSTOM_LOCATION/host/config/dotvimrc ~/.vimrc
 
 # link tmux conf
-# [ ! -e ~/.tmux.conf ] && ln -s $CUSTOM_LOCATION/host/config/dottmux.conf ~/.tmux.conf
-[ ! -d ~/.oh-my-zsh ] && ln -s /opt/oh-my-zsh ~/.oh-my-zsh
+[ ! -e ~/.tmux-themepack ] && ln -s $CUSTOM_LOCATION/host/config/tmux-themepack ~/.tmux-themepack
+[ ! -e ~/.tmux.conf ] && ln -s $CUSTOM_LOCATION/host/config/dottmux.conf ~/.tmux.conf
 
 # touch ~/.sudo_as_admin_successful
